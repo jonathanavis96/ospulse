b8a654549b71f140010d1860021e62bbecb05ae8
jonathanavis96 <jonathanavis96@gmail.com>
2026-07-10 07:38:35 +0200

    style(combat): drop unused ArrayList import in requirement repo

--- src/main/java/com/ospulse/combat/MonsterCombatRequirementRepository.java
+++ src/main/java/com/ospulse/combat/MonsterCombatRequirementRepository.java
@@ -8,7 +8,6 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.UncheckedIOException;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.EnumSet;
 import java.util.HashMap;
